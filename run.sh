e17a98874eee0fe51dd0493336db3e2bf8f66ac7
Slava Shklyaev <slava@slava.sh>
2018-04-07 01:30:22 +0300

    Remove Python strategy

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export DEBUG_STRATEGY=1
-#python3 "$(dirname "$0")/src/main.py"
-python3 -m cProfile -o "$(dirname "$0")/profile.txt" "$(dirname "$0")/src/main.py"
+cd "$(dirname "$0")"
+./target/debug/strategy
