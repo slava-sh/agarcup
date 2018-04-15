776ab64e137af100eae06da53eb47549e2f7b278
Slava Shklyaev <slava@slava.sh>
2018-04-16 00:38:43 +0300

    Update benchmark

--- benches/can_eat_blob.rs
+++ benches/can_eat_blob.rs
@@ -11,33 +11,40 @@ use my_strategy::config::Config;
 fn bench(c: &mut Criterion) {
     Config::default().init_singleton();
     c.bench_function("Player::can_eat_blob", |b| {
-        let m = 300.0;
-        let player = Player {
-            id_: PlayerBlobId {
-                player_id: 1,
-                fragment_id: 0,
-            },
-            point_: Point::new(100.0, 200.0),
-            m_: m,
-            r_: Player::mass_to_radius(m),
-            v_: Default::default(),
-            is_fast_: Default::default(),
-            ttf_: Default::default(),
-        };
-        let m = 80.0;
-        let other = Player {
-            id_: PlayerBlobId {
-                player_id: 2,
-                fragment_id: 0,
-            },
-            point_: Point::new(100.0, 200.0),
-            m_: m,
-            r_: Player::mass_to_radius(m),
-            v_: Default::default(),
-            is_fast_: Default::default(),
-            ttf_: Default::default(),
-        };
-        b.iter(|| player.can_eat_blob(&other))
+        b.iter(|| {
+            (0..100)
+                .map(|i| {
+                    let i = i as f64;
+                    let m = 300.0 + i;
+                    let player = Player {
+                        id_: PlayerBlobId {
+                            player_id: 1,
+                            fragment_id: 0,
+                        },
+                        point_: Point::new(100.0 + i, 200.0 + i),
+                        m_: m,
+                        r_: Player::mass_to_radius(m),
+                        v_: Default::default(),
+                        is_fast_: Default::default(),
+                        ttf_: Default::default(),
+                    };
+                    let m = 80.0 - i;
+                    let other = Player {
+                        id_: PlayerBlobId {
+                            player_id: 2,
+                            fragment_id: 0,
+                        },
+                        point_: Point::new(100.0 - i, 200.0 - i),
+                        m_: m,
+                        r_: Player::mass_to_radius(m),
+                        v_: Default::default(),
+                        is_fast_: Default::default(),
+                        ttf_: Default::default(),
+                    };
+                    player.can_eat_blob(&other) as i64
+                })
+                .sum::<i64>()
+        })
     });
 }
 
